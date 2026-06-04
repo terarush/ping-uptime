e2de4ff62a8273ecb8767b1a3eb5c8c6c7d42a03
rafia9005 <rafia9005@gmail.com>
2026-06-04 12:23:55 +0700

    fix: change script to script setup in Index.vue to resolve ThemeButton

--- web/src/views/Index.vue
+++ web/src/views/Index.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import ThemeButton from '@/components/theme-button.vue'
+</script>
+
 <template>
   <div>
   	<h1>hello world</h1>
+   <ThemeButton />
   </div>
 </template>
