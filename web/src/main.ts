83adedce290d8293700d95783847237fa8b10cc1
rafia9005 <rafia9005@gmail.com>
2026-06-04 12:25:20 +0700

    fix: change PrimeVue darkModeSelector to match my-app-dark class

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,11 +3,19 @@ import './assets/globals.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from "primevue/config"
+import Aura from "@primeuix/themes/aura"
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(PrimeVue)
+app.use(PrimeVue, {
+	theme: {
+		preset: Aura,
+		options: {
+			darkModeSelector: ".my-app-dark"
+		}
+	}
+})
 
 app.mount('#app')
