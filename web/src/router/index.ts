954c88e81d8d943f704b49055d9f989f1195ea3c
rafia9005 <rafia9005@gmail.com>
2026-06-04 18:47:36 +0700

    feat(web): add 404 page NotFound view and register wildcard route

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -32,6 +32,11 @@ const router = createRouter({
         },
       ],
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('@/views/NotFound.vue'),
+    },
   ],
 })
 
