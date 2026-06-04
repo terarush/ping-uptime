349dd174cfe667cca7a0270880fd645cdea7880b
rafia9005 <rafia9005@gmail.com>
2026-06-04 12:16:57 +0700

    feat: restructure frontend project, setup lucide-icons and clean boilerplate files

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import './assets/globals.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from "primevue/config"
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
